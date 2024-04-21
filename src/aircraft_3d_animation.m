85228761004bd261e99ff3d4abbb413fd1934a4e
Atakan Suslu <atakan.suslu@metu.edu.tr>
2024-04-21 14:39:38 +0300

    Better initialization of graphic objects

--- src/aircraft_3d_animation.m
+++ src/aircraft_3d_animation.m
@@ -109,7 +109,7 @@ cameratoolbar('Show')
 % Aircraft transformation group handle
 AV_hg         = hgtransform;
 % controls_deflection_deg transformation group handles
-CONT_hg       = zeros(1,length(Model3D.Control));
+CONT_hg       = gobjects(1,length(Model3D.Control));
 for i=1:length(Model3D.Control)
     CONT_hg(i) = hgtransform('Parent', AV_hg, 'tag', Model3D.Control(i).label);
 end
@@ -124,7 +124,7 @@ euler_hgt(7)  = hgtransform('Parent', euler_hgt(4), 'tag', 'heading_line');
 % Plot objects
 % -------------------------------------------------------------------------
 % Plot airframe
-AV = zeros(1, length(Model3D.Aircraft));
+AV = gobjects(1, length(Model3D.Aircraft));
 for i = 1:length(Model3D.Aircraft)
     AV(i) = patch(Model3D.Aircraft(i).stl_data,  ...
         'FaceColor',        Model3D.Aircraft(i).color, ...
@@ -135,7 +135,7 @@ for i = 1:length(Model3D.Aircraft)
         'Parent',            AV_hg, ...
         'LineSmoothing', 'on');
 end
-CONT = zeros(1, (length(Model3D.Control)));
+CONT = gobjects(1, (length(Model3D.Control)));
 % Plot controls_deflection_deg
 for i=1:length(Model3D.Control)
     CONT(i) = patch(Model3D.Control(i).stl_data,  ...
